e156eb11bd4e5f513ae3dc0a34fab5ed0568ce0c
tolgadonmez <donmeztolga1907@gmail.com>
2021-02-20 01:28:36 +0300

    10.gün 4.ödev

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,28 +10,28 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add(new Entities.Concrete.Car { CarId = 2, BrandId = 1, ColorId = 1, DailyPrice = 120, ModelYear = 2010, Description = "Audi" });
+            //carManager.Add(new Entities.Concrete.Car { CarId = 2, BrandId = 1, ColorId = 1, DailyPrice = 120, ModelYear = 2010, Description = "Audi" });
 
-             Console.WriteLine("CarId \t BrandId \t ColorId \t DailyPrice \t ModelYear \t Description");
-             Console.WriteLine("----------------------------------------------------------------------");
+            // Console.WriteLine("CarId \t BrandId \t ColorId \t DailyPrice \t ModelYear \t Description");
+            // Console.WriteLine("----------------------------------------------------------------------");
            
-            foreach (var car in carManager.GetAll().Data)
-            {
-                Console.WriteLine(car.CarId + "\t" + car.BrandId + "\t" + car.ColorId + "\t" + car.DailyPrice + "\t  " + car.ModelYear + "\t" + car.Description);
-            }
+            //foreach (var car in carManager.GetAll().Data)
+            //{
+            //    Console.WriteLine(car.CarId + "\t" + car.BrandId + "\t" + car.ColorId + "\t" + car.DailyPrice + "\t  " + car.ModelYear + "\t" + car.Description);
+            //}
 
-            var result = carManager.GetCarDetails();
-             if(result.Success == true)
-             {
-                 foreach (var car in result.Data )
-                 {
-                     Console.WriteLine(car.CarName + "  " + car.BrandName + "  " + car.ColorName + "  " + car.DailyPrice);
-                 }
-             }
-             else
-             {
-                 Console.WriteLine(result.Message);
-             }
+            //var result = carManager.GetCarDetails();
+            // if(result.Success == true)
+            // {
+            //     foreach (var car in result.Data )
+            //     {
+            //         Console.WriteLine(car.CarName + "  " + car.BrandName + "  " + car.ColorName + "  " + car.DailyPrice);
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine(result.Message);
+            // }
         }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,40 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, ReCapContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var result = from u in context.Users
+                             join cus in context.Customers
+                             on u.UserId equals cus.UserId
+                             from car in context.Cars
+                             join r in context.Rentals
+                             on car.CarId equals r.CarId
+                             select new RentalDetailDto
+                             {
+                                 Id = r.RentalId,
+                                 CarName = car.Description,
+                                 CustomerName = cus.CompanyName,
+                                 CarId = car.CarId,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate
+                             };
+
+                return result.ToList();
+            }
+        }
+    }
+
+}
